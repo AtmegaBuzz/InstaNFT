58ae27ef1d33345d3551dcdbcffc7ab98bad212a
AtmegaBuzz <swapnilshinde9382@gmail.com>
2024-06-17 14:36:39 +0530

    fix PSRAM init error

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,102 @@
 #include <Arduino.h>
+#include "WiFi.h"
 
-// put function declarations here:
-int myFunction(int, int);
+const char *ssid = "1";
+const char *password = "1";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+camera_config_t config;
+int camera_init_fail = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Wifi Connected");
+
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_HD;
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  // config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 2;
+  config.grab_mode = CAMERA_GRAB_LATEST;
+
+  esp_err_t err = esp_camera_init(&config);
+
+  if (err != ESP_OK)
+  {
+    camera_init_fail = 1;
+    Serial.printf("Camera init failed with error 0x%x", err);
+    Serial.println("");
+  }
+
+  // while(esp_camera_init(&config) != ESP_OK) {
+  //     delay(4000);
+  // }
+
+  sensor_t *s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID)
+  {
+    s->set_vflip(s, 1);       // flip it back
+    s->set_brightness(s, 5);  // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  if (config.pixel_format == PIXFORMAT_JPEG)
+  {
+    s->set_framesize(s, FRAMESIZE_QVGA);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  if (!camera_init_fail)
+  {
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    Serial.println("Taking picture ...");
+
+    camera_fb_t *pic = esp_camera_fb_get();
+
+    Serial.print("Took a picture of size: ");
+    Serial.println(pic->len);
+
+    esp_camera_fb_return(pic);
+  }
+
+  else
+  {
+    Serial.println("Camera failed to init");
+  }
+
+  delay(2000);
 }
